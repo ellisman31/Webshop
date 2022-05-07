9932ea94a510efd34f0c30e47a35f51ca0d35feb
ellisman31 <akos99@protonmail.com>
2022-05-07 23:56:37 +0200

    setup(#1): Implement JPA repository for every entity

--- backend/src/main/java/com/webshop/app/JPARepository/CustomerJPARepository.java
+++ backend/src/main/java/com/webshop/app/JPARepository/CustomerJPARepository.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.webshop.app.JPARepository;
 
+import com.webshop.app.Model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- *
- * @author pc
- */
-public interface CustomerJPARepository extends JpaRepository{
+public interface CustomerJPARepository extends JpaRepository<Customer, Long>{
     
 }
