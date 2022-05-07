9c9d031fa45dbaf72191c18506bdf2d0a1754f1c
ellisman31 <akos99@protonmail.com>
2022-05-07 16:30:55 +0200

    setup(#1): default classes and folders creation for the project

---
+++ backend/src/main/java/com/webshop/app/JPARepository/ProductJPARepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.webshop.app.JPARepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author pc
+ */
+public interface ProductJPARepository extends JpaRepository{
+    
+}
