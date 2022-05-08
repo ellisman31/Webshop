078dffc369becbdfaab547bfb4f636604eab6cf0
ellisman31 <akos99@protonmail.com>
2022-05-08 02:19:06 +0200

    setup(#4): add repository annotation for jpa repositories

--- backend/src/main/java/com/webshop/app/JPARepository/ProductJPARepository.java
+++ backend/src/main/java/com/webshop/app/JPARepository/ProductJPARepository.java
@@ -2,7 +2,9 @@ package com.webshop.app.JPARepository;
 
 import com.webshop.app.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductJPARepository extends JpaRepository<Product, Long>{
     
 }
