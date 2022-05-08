b90aa2903dd442c0b7f93e2970be1f27e7871dcf
ellisman31 <akos99@protonmail.com>
2022-05-08 23:41:20 +0200

    feat(#6): Customer Controller implementation with Service

--- backend/src/main/java/com/webshop/app/Controller/CustomerController.java
+++ backend/src/main/java/com/webshop/app/Controller/CustomerController.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.webshop.app.Controller;
 
-/**
- *
- * @author pc
- */
+import com.webshop.app.Model.Customer;
+import com.webshop.app.Model.Product;
+import com.webshop.app.Service.CustomerService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class CustomerController {
+    
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+    
+    @RequestMapping(value = "/getAllCustomer", method = RequestMethod.GET)
+    public List<Customer> getAllCustomer(){
+        return customerService.getAllCustomer();
+    }
+    
+    @RequestMapping(value = "/getCustomer/{customerId}", method = RequestMethod.GET)
+    public Optional<Customer> getCustomerById(@PathVariable Long customerId) {
+        return customerService.getCustomerById(customerId);
+    }
+    
+    @RequestMapping(value = "/addCustomer", method = RequestMethod.POST)
+    public void addCustomer(Customer customer) {
+        customerService.addCustomer(customer);
+    }
+    
+    @RequestMapping(value = "/deleteCustomer/{customerId}", method = RequestMethod.DELETE)
+    public void deleteCustomer(@PathVariable Long customerId) {
+        customerService.deleteCustomerById(customerId);
+    }
+    
+    @RequestMapping(value = "/updateCustomer/{customerId}", method = RequestMethod.PUT)
+    public void updateCustomer(@PathVariable Long customerId, Customer customer) {
+        customerService.updateCustomerById(customerId, customer);
+    }
+    
+    @RequestMapping(value = "/addProductToCart/{customerId}", method = RequestMethod.PUT)
+    public void addProductToCart(@PathVariable Long customerId, Product product) {
+        customerService.addProductToCart(customerId, product);
+    }
+    
+    @RequestMapping(value = "/getCustomerCart/{customerId}", method = RequestMethod.GET)
+    public List<Product> getCustomerCart(@PathVariable Long customerId) {
+        return customerService.getCustomerCart(customerId);
+    }
     
 }
