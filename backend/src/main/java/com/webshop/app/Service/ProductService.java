c65d643849eeca9c457a097c346327c248534315
ellisman31 <akos99@protonmail.com>
2022-05-08 03:01:21 +0200

    feat(#4): implement methods and make connection to receive and send changes to database

--- backend/src/main/java/com/webshop/app/Service/ProductService.java
+++ backend/src/main/java/com/webshop/app/Service/ProductService.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.webshop.app.Service;
 
-/**
- *
- * @author pc
- */
-public class ProductService {
+import com.webshop.app.JPARepository.ProductJPARepository;
+import com.webshop.app.Model.Product;
+import com.webshop.app.ServiceInterface.ProductServiceInterface;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService implements ProductServiceInterface{
+
+    private final ProductJPARepository productJPA;
     
+    @Autowired
+    public ProductService(ProductJPARepository productJPA) {
+        this.productJPA = productJPA;
+    }
+    
+    
+    @Override
+    public List<Product> getAllProduct() {
+        return productJPA.findAll();
+    }
+
+    @Override
+    public Optional<Product> getProductById(Long productId) {
+        return productJPA.findById(productId);
+    }
+
+    @Override
+    public void deleteProductById(Long productId) {
+        productJPA.deleteById(productId);
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+        productJPA.save(product);
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        productJPA.save(product);
+    }
+
+        
 }
